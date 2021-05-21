e0543dc94698bbcf6e09a4a0149a93a135c794c2
James Allen <jayallen51@hotmail.co.uk>
2021-05-21 20:54:50 +0100

    attempting star

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "instance_ip_addr" {
   count = var.instance_count
-  value       = google_compute_address.vm_static_ip[count.index].address
+  value       = google_compute_address.vm_static_ip.*.address
   description = "The private IP address of the main server instance."
 }
