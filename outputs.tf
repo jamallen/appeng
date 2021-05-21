73eeccddd973e32a506c5a624f6aa622543af92a
James Allen <jayallen51@hotmail.co.uk>
2021-05-21 20:55:30 +0100

    remove count line

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,4 @@
 output "instance_ip_addr" {
-  count = var.instance_count
   value       = google_compute_address.vm_static_ip.*.address
   description = "The private IP address of the main server instance."
 }
