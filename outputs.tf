9e69a9caea97468524cbc3eab5623b45edec5c41
James Allen <jayallen51@hotmail.co.uk>
2021-05-21 20:51:03 +0100

    count addition to output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "instance_ip_addr" {
-  value       = google_compute_address.vm_static_ip.address
+  value       = google_compute_address.vm_static_ip.address[count.index]
   description = "The private IP address of the main server instance."
 }
