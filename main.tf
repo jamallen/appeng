758d527a856abb9e4cd73e6a2ea6d938b97f2dc0
James Allen <jayallen51@hotmail.co.uk>
2021-05-21 14:29:51 +0100

    addition of tag

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+
+  credentials = file("gcpterraform.json")
+  project = "serious-octagon-314208"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags = ["web","dev","small"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+        nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
+}
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
