0d9e08c588e0c979289897bbc5828050a36a0669
James Allen <jayallen51@hotmail.co.uk>
2021-05-21 20:53:14 +0100

    adding instance count

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "instance_ip_addr" {
+  count = var.instance_count
   value       = google_compute_address.vm_static_ip[count.index].address
   description = "The private IP address of the main server instance."
 }
