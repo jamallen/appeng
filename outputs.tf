c53fae15e8d235c37a1afa39e4ee9e9553f74427
James Allen <jayallen51@hotmail.co.uk>
2021-05-21 20:51:58 +0100

    countindex added correctly

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "instance_ip_addr" {
-  value       = google_compute_address.vm_static_ip.address[count.index]
+  value       = google_compute_address.vm_static_ip[count.index].address
   description = "The private IP address of the main server instance."
 }
