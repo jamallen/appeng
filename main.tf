d94054a3b7466002e5fc03b51c1a7825d139eb6b
James Allen <jayallen51@hotmail.co.uk>
2021-05-21 22:34:22 +0100

    rewrite of welcome message

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 
-  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Hello from ${google_compute_instance.vm_instance[count.index].name} Terraform on Google Cloud!</h1></body></html>' | sudo tee /var/www/html/index.html"
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Hey App Team I Am One Of Your New Machines</h1></body></html>' | sudo tee /var/www/html/index.html"
 
   network_interface {
     network = google_compute_network.vpc_network.name
